39f61356de988798c581c9e5410dc97b4e8212b4
Ross Brown <ross@cremalab.com>
2012-04-28 10:29:24 -0500

    Visual fart feedback
    TODO: Haptic feedback

--- public/js/poop_listener.js
+++ public/js/poop_listener.js
@@ -4,12 +4,13 @@
   socket = io.connect('/');
 
   $(function() {
-    var all_fart_sounds, build_fart, default_sound, dom_audio, dom_farters, file_path, file_type;
+    var all_fart_sounds, build_fart, default_sound, dom_audio, dom_farters, file_path, file_type, mascot;
     default_sound = 'fart-1';
     file_type = 'wav';
     file_path = '/sounds/';
     dom_audio = $('audio');
     dom_farters = $('.latest_farters').find('ul');
+    mascot = $('.poop');
     all_fart_sounds = [];
     $('ul.all_fart_sounds').find('li').each(function() {
       return all_fart_sounds.push($(this).text());
@@ -21,7 +22,7 @@
       return build_fart(data);
     });
     return build_fart = function(data) {
-      var fart_sound, rand, template;
+      var callback, fart_sound, rand, template;
       template = "<li><a target='_blank' href='http://twitter.com/" + data.user + "'><img src='" + data.img + "' alt='@" + data.user + "' /></a><a target='_blank' href='http://twitter.com/" + data.user + "'>@" + data.user + "</a> : " + data.content + "</li>";
       dom_farters.prepend(template);
       fart_sound = '';
@@ -35,6 +36,11 @@
       		else
       			fart_sound = file_path + default_sound + '.' + file_type
       */
+      mascot.addClass('pooped');
+      callback = function() {
+        return mascot.removeClass('pooped');
+      };
+      setTimeout(callback, 600);
       dom_audio.empty();
       dom_audio.append('<source src="' + fart_sound + '" type="audio/' + file_type + '" />');
       return dom_audio[0].play();
